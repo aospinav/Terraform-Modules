90b12cedb21355c6aa1dcdc9b80acc94822a7d2e
Alejandro ospina <11dimensiones@gmail.com>
2025-10-22 00:16:35 -0500

    changing name of modules

--- resources.tf
+++ resources.tf
@@ -1,4 +1,4 @@
-module "networking_settings" {
+module "network" {
   source = "./modules/network"
 
   name_vpc      = var.name_vpc
@@ -9,13 +9,13 @@ module "networking_settings" {
 
 }
 
-module "security_settings" {
+module "network_security" {
   source = "./modules/network_security"
 
   name_sg_http         = var.name_sg_http
   name_sg_ssh          = var.name_sg_ssh
   name_sg_private_http = var.name_sg_private_http
-  vpc_id               = module.networking_settings.vpc_id
+  vpc_id               = module.network.vpc_id
   allowed_ip_range     = var.allowed_ip_range
   custom_ip            = var.custom_ip
 
@@ -28,15 +28,15 @@ module "application" {
   name_lt       = var.name_lt
   type_instance = var.type_instance
   sg_instance_id = [
-    module.security_settings.sg_private_http_id,
-  module.security_settings.sg_ssh_id]
+    module.network_security.sg_private_http_id,
+  module.network_security.sg_ssh_id]
   name_asg           = var.name_asg
-  public_subnetA_id  = module.networking_settings.subnet_a_id
-  public_subnetB_id  = module.networking_settings.subnet_b_id
-  public_subnetC_id  = module.networking_settings.subnet_c_id
+  public_subnetA_id  = module.network.subnet_a_id
+  public_subnetB_id  = module.network.subnet_b_id
+  public_subnetC_id  = module.network.subnet_c_id
   name_load_balancer = var.name_load_balancer
-  sg_lb_id           = [module.security_settings.sg_http_id]
-  vpc_id             = module.networking_settings.vpc_id
+  sg_lb_id           = [module.network_security.sg_http_id]
+  vpc_id             = module.network.vpc_id
 
 }
 
